b9b7cb5eb3a5ecc2bb5b9cc5a8291914a5278628
David Begin <davidmichaelbe@gmail.com>
2015-05-01 21:21:46 -0700

    trying out some specs on some fake http code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,50 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+struct HttpResponse {
+    status: u16,
+    headers: String,
+    body: String,
+}
+
+
+fn response_validator(response: HttpResponse) -> Result<&'static str, &'static str> {
+    if response.status == 200 {
+        Ok("nice response")
+    } else if response.status == 404 {
+        Err("Cool an Error")
+    } else {
+        Ok("Im like, whatever")
+    }
+}
+
+#[test]
+fn it_returns_a_good_string_for_a_200_response() {
+    let raw_response = HttpResponse {
+        status  : 200,
+        headers : "{}".to_string(),
+        body    : "{}".to_string(),
+    };
+
+    assert_eq!(response_validator(raw_response).unwrap(), "nice response")
+}
+
+#[test]
+fn it_returns_a_bad_string_for_404_response() {
+    let raw_response = HttpResponse {
+        status: 404,
+        headers: "{}".to_string(),
+        body: "{}".to_string(),
+    };
+
+    let response = response_validator(raw_response);
+
+    match response {
+        Ok(v) => assert!(false),
+        Err(e) => assert!(true, "Hey 404's are supposed to be Err's"),
+    }
+}
+
 #[test]
 fn it_works() {
 }
--- src/main.rs
+++ src/main.rs
@@ -7,42 +7,18 @@ mod the_printers;
 fn main() {
     the_printers::headers::print_title();
 
-    // I want to work with http requests
+    // let response = HttpResponse {
+    //     status: 200,
+    //     headers: "{}".to_string(),
+    //     body: "<html></html>".to_string()
+    // };
     //
-    // we need 3 things
+    // let result = response_validator(response);
     //
-    // status code
-    //
-    // headers
-    //
-    // body
-
-    let response = HttpResponse {
-        status: 200u16,
-        headers: "{}".to_string(),
-        body: "<html></html>".to_string()
-    };
-
-    // So I now need a function that takes an HttpResponse
-    // and returns a Result
-    //
-    // and then I can branch on that result
-
-    let result = response_validator(response);
-
-    if result.is_ok() {
-        println!("successful response");
-    } else if result.is_err() {
-        println!("bad response");
-    }
+    // if result.is_ok() {
+    //     println!("successful response");
+    // } else if result.is_err() {
+    //     println!("bad response");
+    // }
 }
 
-struct HttpResponse {
-    status: u16,
-    headers: String,
-    body: String,
-}
-
-fn response_validator(response: HttpResponse) -> Result<&'static str, &'static str> {
-    Ok("durf")
-}
