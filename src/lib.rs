c877fe8f8f348e1e14daa4fe99c1b0981dabca75
David Begin <davidmichaelbe@gmail.com>
2015-05-01 21:41:44 -0700

    so you add pub to fix compiler issues
      ...ok cool that makes sense

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,31 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-struct HttpResponse {
-    status: u16,
-    headers: String,
-    body: String,
-}
 
-fn response_validator(response: HttpResponse) -> Result<&'static str, &'static str> {
-    if response.status == 200 {
-        Ok("nice response")
-    } else if response.status == 404 {
-        Err("Cool an Error")
-    } else {
-        Ok("Im like, whatever")
+// Httparty and Faraday combined!
+//
+// ruby everyone (applause)!
+mod http_day {
+    pub struct HttpResponse {
+        pub status: u16,
+        pub headers: String,
+        pub body: String,
+    }
+
+    pub fn response_validator(response: HttpResponse) -> Result<&'static str, &'static str> {
+        if response.status == 200 {
+            Ok("nice response")
+        } else if response.status == 404 {
+            Err("Cool an Error")
+        } else {
+            Ok("Im like, whatever")
+        }
     }
 }
 
-
 #[cfg(test)]
 mod test {
-    use super::HttpResponse;
-    use super::response_validator;
+    use super::http_day::*;
 
     #[test]
     fn it_returns_a_good_string_for_a_200_response() {
